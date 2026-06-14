17f636970d4c4645fd30b6095db920e2a5aeb84e
mayalinux <maya.linux.2024@gmail.com>
2026-06-14 17:18:23 +0300

    Complete Docker multi-stage build + running Flask container

--- 02-docker/app/app.py
+++ 02-docker/app/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Cloud Native DevOps Platform"
 
+@app.route("/health")
+def health():
+    return {"status": "healthy"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
